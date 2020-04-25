2994db08c8d83504ffd45f76a16d8d8fd1788c2e
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-04-25 21:37:51 +0200

    03 - View Model MVVM Basics. A button is added and bound to Class1. TestProp refreshing solution 2: PropertyChanged event in property.

--- WpfTreeView/Class1.cs
+++ WpfTreeView/Class1.cs
@@ -5,6 +5,8 @@ namespace WpfTreeView
 {
     public class Class1 : INotifyPropertyChanged // --> From this Interface this class is a ViewModel
     {
+        private string mTest;
+
         public event PropertyChangedEventHandler PropertyChanged  = (sender, e) => { };
 
         //if we write in the MainWindow.xaml <Button Content="{Binding}"/> then content wants to write string, which is the name of the class1. Instead,
@@ -14,7 +16,23 @@ namespace WpfTreeView
             return "Hello Word!";
         }
 
-        public string TestProp { get; set; } = "My Test Property";
+        public string TestProp
+        {
+            get
+            {
+                return mTest;
+            }
+            set
+            {
+                if (mTest == value)
+                    return;
+
+                mTest = value;
+                //In order that "TestProp" is refreshed:
+                //PropertyChanged(this, new PropertyChangedEventArgs("TestProp")); //This will fire the PropertyChangedEventHandler event.
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(TestProp))); //This will fire the PropertyChangedEventHandler event.
+            }
+        } 
 
         public Class1()
         {
@@ -25,9 +43,7 @@ namespace WpfTreeView
                 {
                     await Task.Delay(200);
                     TestProp = (i++).ToString();
-                    //In order that "TestProp" is refreshed:
-                    PropertyChanged(this, new PropertyChangedEventArgs("TestProp")); //This will fire the PropertyChangedEventHandler event.
-                }
+                 }
             });
         }
     }
